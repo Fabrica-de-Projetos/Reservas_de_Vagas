d01c498005622293fb70f0cdfc94beb6a1b9c446
Gabriel Erick <gabrielmmve13@gmail.com>
2026-04-10 18:33:37 -0300

    permitindo inserção de dados em massa na tabela usuários + método de validação, criação e retorno em json na tabela de usuários

--- backend/app/Http/Controllers/UsuarioController.php
+++ backend/app/Http/Controllers/UsuarioController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// namespace que me permite usar método de criptografia de senha em hash
+use Illuminate\Support\Facades\Hash;
+use App\Models\Usuario;
+use League\Config\Exception\ValidationException;
 
 class UsuarioController extends Controller
 {
@@ -27,7 +31,41 @@ class UsuarioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'nome' => 'required|string|max:100',
+                'email' => 'required|email|unique:usuarios,email',
+                'senha' => 'required|min:6',
+                'telefone' => 'required|string|min:14|max:15|unique:usuarios,telefone', //minimo de 15 caracteres devido a ddd e máscara de formatação (xx) xxxx-xxxx
+                'cidade' => 'required|string|max:50',
+                'estado' => 'required|string|max:2'
+            ]);
+
+            $usuario = Usuario::create([
+                'nome' => $request->nome,
+                'email' => $request->email,
+                'senha' => Hash::make($request->senha),
+                'telefone' => $request->telefone,
+                'cidade' => $request->cidade,
+                'estado' => $request->estado
+            ]);
+
+            return response()->json([
+                'message' => 'Usuário criado com sucesso',
+                'data' => $usuario
+            ], 201);
+        } catch (ValidationException $e) { // se der um erro do tipo "ValidationException" cai nesse catch
+            /** @var ValidationException $e */
+            return response()->json([
+                'message' => 'Erro de validação.',
+                'errors' => $e->errors()
+            ], 422);
+        } catch (\Throwable $e) { // qualquer outro tipo de erro, exceção no try que implemente a interface "Throwable" cai aqui
+            return response()->json([
+                'message' => 'Erro interno no servidor.',
+                'error' => $e->getMessage()  // tacando o erro 100% crú msm por enquanto
+            ], 500);
+        }
     }
 
     /**
--- backend/app/Models/Usuario.php
+++ backend/app/Models/Usuario.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+        'telefone',
+        'cidade',
+        'estado'
+    ];
 }
