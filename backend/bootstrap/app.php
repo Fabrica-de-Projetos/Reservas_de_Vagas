92d1ff5325a7db9cae0eb438886a4e05155b4cdf
Gabriel Erick <gabrielmmve13@gmail.com>
2026-04-11 20:42:25 -0300

    seeder para inserção de usuários + método para retornar todos os usuários + teste de rota

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        // garantindo que o laravel carregue na sua inicialização (por isso na pasta bootstrap) meu arquivo que contem minhas rotas de api, assim como ele carrega a linha acima (web.php)
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
