d2bc1d7ee4ace6194a88fed04aee1d148e65146c
Gabriel Erick <gabrielmmve13@gmail.com>
2026-04-15 17:20:37 -0300

    rota e controller de Login

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\UsuarioNaoEncontradoException;
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'senha' => 'required'
+        ]);
+
+        $usuario = Usuario::where('email', $request->email)->first();
+
+        if ($usuario == false || Hash::check($request->senha, $usuario->senha) == false){
+            return response()->json([
+                'message' => 'Email ou senha inválidos.'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Usuário autenticado com sucesso.',
+            'usuario' => [
+                'id' => $usuario->id,
+                'nome' => $usuario->nome_usuario,
+                'email' => $usuario->email
+            ]
+        ], 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{UsuarioController, EstacionamentoController, ReservaController, VagaController, VeiculoController};
-
+use App\Http\Controllers\{UsuarioController, EstacionamentoController, ReservaController, VagaController, VeiculoController, LoginController};
 /* 
 ! observação importante: rotas ganham prefixo de "/api" por estarem neste arquivo de api, logo, um exemplo de rota correta (por exemplo, para usuarios) aplicando este prefixo api sem o uso do domínio (http://localhostEvariados) seria: /api/spotLivre/usuarios (utilizadno verbo GET irá me retornar todos os usuários do sistema) e exemplo de rota completa: http://localhost/api/spotLivre/usuarios (verbo get aqui para retornar todos os usuários)
 */
@@ -12,3 +11,5 @@ Route::resource("/spotLivre/reservas", ReservaController::class);
 Route::resource("/spotLivre/usuarios", UsuarioController::class);
 Route::resource("/spotLivre/vagas", VagaController::class);
 Route::resource("/spotLivre/veiculos", VeiculoController::class);
+
+Route::post("/spotLivre/login", [LoginController::class, 'login']);
