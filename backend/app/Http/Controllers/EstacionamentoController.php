14baa199c5003dcacb9781bef26865c0abfd2ba3
Gabriel Erick <gabrielmmve13@gmail.com>
2026-05-10 14:31:51 -0300

    criaćão dos métodos do controller estacionamento

--- backend/app/Http/Controllers/EstacionamentoController.php
+++ backend/app/Http/Controllers/EstacionamentoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Estacionamento;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class EstacionamentoController extends Controller
 {
@@ -15,9 +16,14 @@ class EstacionamentoController extends Controller
         try {
             $estacionamentos = Estacionamento::all();
 
-            return response()->json([$estacionamentos], 200);
+            return response()->json([
+                'estacionamentos' => $estacionamentos
+            ], 200);
         } catch (\Throwable $th) {
-            //throw $th;
+            return response()->json([
+                'message' => 'Não foi possível recuperar os estacionamentos.',
+                'errors' => $th->getMessage()
+            ], 500);
         }
     }
 
@@ -34,7 +40,42 @@ class EstacionamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'nome' => ['required', 'string', 'max:100'],
+                'rua' => ['required', 'string', 'max:100'],
+                'numero' => ['required', 'string', 'max:10'],
+                'bairro' => ['required', 'string', 'max:100'],
+                'cep' => ['required', 'string', 'max:8'],
+                'estado' => ['required', 'max;2'],
+                'total_vagas' => ['required', 'integer']
+            ]);
+
+            $estacionamento = Estacionamento::create([
+                'nome' => $request->nome,
+                'rua' => $request->rua,
+                'numero' => $request->numero,
+                'bairro' => $request->bairro,
+                'cep' => $request->cep,
+                'estado' => $request->estado,
+                'total_vagas' => $request->total_vagas
+            ]);
+
+            return response()->json([
+                'message' => 'Estacionamento criado com sucesso!',
+                'estacionamento' => $estacionamento
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Erro de validação.',
+                'errors' => $e->getMessage()
+            ], 422);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message' => 'Erro interno no servidor.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -42,7 +83,17 @@ class EstacionamentoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $estacionamento = Estacionamento::findOrFail($id);
+            return response()->json([
+                'estacionamento' => $estacionamento
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Não foi possível recuperar o estacionamento.',
+                'errors' => $th->getMessage()
+            ], 404);
+        }
     }
 
     /**
@@ -58,7 +109,44 @@ class EstacionamentoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $estacionamento = Estacionamento::findOrFail($id);
+
+            $request->validate([
+                'nome' => ['sometimes', 'string', 'max:100'],
+                'rua' => ['sometimes', 'string', 'max:100'],
+                'numero' => ['sometimes', 'string', 'max:10'],
+                'bairro' => ['sometimes', 'string', 'max:100'],
+                'cep' => ['sometimes', 'string', 'max:8'],
+                'estado' => ['sometimes', 'max;2'],
+                'total_vagas' => ['sometimes', 'integer']
+            ]);
+
+            $estacionamento->update([
+                'nome' => $request->nome ?? $estacionamento->nome,
+                'rua' => $request->rua ?? $estacionamento->rua,
+                'numero' => $request->numero ?? $estacionamento->numero,
+                'bairro' => $request->bairro ?? $estacionamento->bairro,
+                'cep' => $request->cep ?? $estacionamento->cep,
+                'estado' => $request->estado ?? $estacionamento->estado,
+                'total_vagas' => $request->total_vagas ?? $estacionamento->total_vagas
+            ]);
+
+            return response()->json([
+                'message' => 'Estacionamento atualizado com sucesso!',
+                'estacionamento' => $estacionamento
+            ], 200);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Erro de validação.',
+                'errors' => $e->getMessage()
+            ], 422);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message' => 'Erro interno no servidor.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -66,6 +154,17 @@ class EstacionamentoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            Estacionamento::destroy($id);
+
+            return response()->json([
+                'message' => 'Estacionamento excluído com sucesso!'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Não foi possível excluir o estacionamento.',
+                'errors' => $th->getMessage()
+            ], 204);
+        }
     }
 }
